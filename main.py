ef597e40810dc58cd1c32e4e179f06c342bcc6d5
Mauricio Muñoz <a00832956@itesm.mx>
2021-02-09 04:59:21 +0000

    implemented database

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import random
 from Imgs import card
 import asyncio
+import asyncpg
+import os
 
 description = '''hola'''
 intents = discord.Intents.default()
@@ -10,11 +12,19 @@ intents.members = True
 bot = commands.Bot(command_prefix='_',
                    description=description,
                    intents=intents)
-
+                   
 isActive = False
 savedMessage = None
-channelID = 807399692303859732
+channelID = os.getenv('CHANNEL_ID')
 Leaderboard = {}
+
+emptyArray = [0]*30
+
+async def create_db_pool():
+  bot.pg_con = await asyncpg.create_pool(database="pogUBot", user="admin", password=os.getenv('PASSWORD'))
+
+
+
 
 @bot.event
 async def on_ready():
@@ -23,16 +33,23 @@ async def on_ready():
     print('-------')
 
 
-async def showLeaderboard():
-  leaderboardText = ""
+@bot.event
+async def on_message(message):
+    global isActive
 
-  for person, score in Leaderboard.items():
-    leaderboardText += ("{} -> {} \n".format(person, score))
-  
-  channel = bot.get_channel(channelID)
-  await channel.send(embed = discord.Embed(title="Leaderboard", description=leaderboardText))
+    # print("author: {}, bot: {}, isActive{}".format(message.author, bot.user,  isActive))
 
+    if message.author == bot.user:
+        return
 
+    if "_get" in message.content:
+        await get(message)
+    elif "_leaderboard" in message.content:
+        await showLeaderboard()
+    elif isActive == True:
+        return
+    else:
+      await start()
 
 
 async def get(message):
@@ -46,10 +63,15 @@ async def get(message):
     else:
         isActive = False
 
-        if not str(message.author) in Leaderboard:
-          Leaderboard[str(message.author)] = 1
-        else:
-          Leaderboard[str(message.author)] += 1
+        user = await bot.pg_con.fetch("SELECT * FROM users WHERE user_id = $1", message.author.id)
+
+        if not user:
+          await bot.pg_con.execute("INSERT INTO users (user_id, cards, card_count, money) VALUES ($1, $2, $3, $4)", message.author.id, emptyArray, 0, 0)
+
+        user = await bot.pg_con.fetchrow("SELECT * FROM users WHERE user_id = $1", message.author.id)
+        await bot.pg_con.execute("UPDATE users SET card_count = $1 WHERE user_id = $2", user['card_count'] + 1, message.author.id) # Just updates card count, would be nice to update the specific id card number. TODO
+
+
 
         newEmbed = discord.Embed(title="Nahhhh, el {} me atrapo.".format(
                                 message.author),
@@ -57,6 +79,18 @@ async def get(message):
                                 color=0x00ff00)
         await savedMessage.edit(embed = newEmbed)
 
+
+async def showLeaderboard():
+  top5 = await bot.pg_con.fetch("SELECT * FROM users ORDER BY card_count DESC LIMIT 5")
+  
+  embedText = ""
+
+  for i in range(0,5):
+    embedText += str(top5[i]['user_id']) + " --------> " + str(top5[i]['card_count']) + "\n"
+
+  embed = discord.Embed(title="Leaderboard", description=embedText)
+  channel = bot.get_channel(channelID)
+  await channel.send(embed=embed)
 
 async def start():
     global isActive
@@ -81,23 +115,7 @@ async def printCard(name, link):
     savedMessage = await channel.send(embed=embedVar)
 
 
-@bot.event
-async def on_message(message):
-    global isActive
-
-    print("author: {}, bot: {}, isActive{}".format(message.author, bot.user, isActive))
-
-    if message.author == bot.user:
-        return
-
-    if "_get" in message.content:
-        await get(message)
-    elif "_leaderboard" in message.content:
-        await showLeaderboard()
-    elif isActive == True:
-        return
-    else:
-      await start()
 
 
-bot.run('NzUzMjgwNTgwNzE5ODcwMDQ0.X1j5NQ.TMUs167-m442efTnF8mOhOtNc1Y')
+bot.loop.run_until_complete(create_db_pool())
+bot.run(os.getenv('TOKEN'))
