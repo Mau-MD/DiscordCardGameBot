a0b57ccfe34ffa26f1e5907446f6f556963c5684
Mauricio Muñoz <a00832956@itesm.mx>
2021-02-11 16:55:02 -0800

    added rarity and card visualization

--- card.py
+++ card.py
@@ -2,91 +2,146 @@ import random
 
 
 class Card:
-    def __init__(self, name, link, id):
+    def __init__(self, name, link, id, rareza):
         self.name = name
         self.link = link
         self.id = id
+        self.rareza = rareza
 
 
 cards = [
     Card(
         "Naki Guitar",
         "https://i.imgur.com/mebMzx5.jpg",
-        1),
+        1,
+        5),
     Card(
         "Guitar",
         "https://i.imgur.com/ypVfoUE.jpg",
-        2),
+        2,
+        1),
     Card(
         "Brofist",
         "https://i.imgur.com/PhPcDHS.png",
-        3
+        3,
+        1
     ),
     Card(
         "Carro de Max",
         "https://i.imgur.com/qZK7Fmf.jpg",
-        4
+        4,
+        3
     ),    
     Card(
         "Mau viendo el futuro",
         "https://i.imgur.com/P7JrHDX.jpg",
-        5
+        5,
+        3
     ),
     Card(
         "Hiram siendo hiram",
         "https://i.imgur.com/2NjunhD.jpg",
-        6
+        6,
+        2
     ),
     Card(
         "Mau GRRRRR",
         "https://i.imgur.com/KCn6bup.jpg",
-        7
+        7,
+        4
     ),
     Card(
         "Puro ANAHUAC",
         "https://i.imgur.com/U5K82MG.jpg",
-        8
+        8,
+        2
     ),
     Card(
         "Hiram siendo gay",
         "https://i.imgur.com/Nz8C1TN.png",
-        9
+        9,
+        4
     ),
     Card(
         "Pabel sexy",
         "https://i.imgur.com/gA0sMev.jpg",
-        10
+        10,
+        2
     ),
     Card(
         "Pasaporte de Tomas",
         "https://i.imgur.com/gEJWSwj.png",
-        11
+        11,
+        1
     ),
     Card(
         "guapeton haciendo brofist",
         "https://i.imgur.com/OSESWhb.png",
-        12
+        12,
+        3
     ),
     Card(
         "pabel brofist",
         "https://i.imgur.com/Hsj8joh.jpg",
-        13
+        13,
+        2
     ),
     Card(
         "Brofist maximus",
         "https://i.imgur.com/DybRG0i.jpg",
-        14
+        14,
+        5
     ),
     Card(
         "Inspector de chichis",
         "https://i.imgur.com/UwCyqrh.jpg",
-        15
+        15,
+        4
     ),
     Card(
-      "THANOSINTHEBOX BEING A GOD",
-      "https://media.discordapp.net/attachments/807399692303859732/807859220145504306/RobloxScreenShot20201225_232307832.png?width=860&height=484",
-      16
-    )
+        "THANOSINTHEBOX BEING A GOD",
+        "https://media.discordapp.net/attachments/807399692303859732/807859220145504306/RobloxScreenShot20201225_232307832.png?width=860&height=484",
+        16,
+        5
+    ),
+    Card(
+        "Los 3 mosqueteros",
+        "https://media.discordapp.net/attachments/807409001964765194/808521062873694269/image0.jpg?width=645&height=484",
+        17,
+        3
+    ),
+    Card(
+        "AngelLoLXD",
+        "https://media.discordapp.net/attachments/807409001964765194/808521471579258880/image0.jpg?width=363&height=484",
+        18,
+        4
+    ),
+    Card(
+        "Naki Tryhard",
+        "https://media.discordapp.net/attachments/807409001964765194/808522672601825290/image0.jpg?width=363&height=484",
+        19,
+        4
+    ),
+    Card(
+        "Vegeta Sus",
+        "https://media.discordapp.net/attachments/807409001964765194/808523040618315786/image0.jpg?width=393&height=484",
+        20,
+        5
+    ),
+    Card(
+        "Duo Dab",
+        "https://media.discordapp.net/attachments/807409001964765194/808523807698452480/image0.jpg",
+        21,
+        4
+    ),
+    Card(
+        "Duo Faze up",
+        "https://media.discordapp.net/attachments/807409001964765194/808523807873826837/image1.jpg?width=272&height=484",
+        22,
+        5
+    ),
+
+
   
 
 ]
@@ -94,4 +149,4 @@ cards = [
 
 def getRandomCard():
     index = random.randint(0, len(cards) - 1)
-    return cards[index].name, cards[index].link, cards[index].id
+    return cards[index].name, cards[index].link, cards[index].id, cards[index].rareza
--- cogs/images.py
+++ cogs/images.py
@@ -11,10 +11,12 @@ class Images(commands.Cog):
     savedMessage = None
     channelID = TOKENS.CHANNEL_ID
     isActive = False
-    emptyArray = [0]*20
+    emptyArray = [0]*100
 
     minTime = 30
     maxTime = 180
+
+    currentCard = card.Card("","",-1,-1)
 
     def __init__(self, bot):
         self.bot = bot
@@ -29,11 +31,50 @@ class Images(commands.Cog):
         timer = random.randint(Images.minTime, Images.maxTime)
 
         await asyncio.sleep(timer)
-        name, link, ID = card.getRandomCard()
 
-        print(f"{id} - {name}: {link} ")
+        # 1 a 5 es exepcional
+        # 6 a 15 es leyenda
+        # 26 a 40 es raro
+        # 41 a 60 es uncomun
+        # 61 a 100 es comun
 
-        await self.print_card(name, link)
+        indiceRareza = random.randint(1, 100)
+        print(indiceRareza)
+        color = 0xF0F3F4
+
+        if 1 <= indiceRareza <= 5:
+            name, link, ID, rareza = card.getRandomCard()
+            while rareza != 5:
+                name, link, ID, rareza = card.getRandomCard()
+            color = 0xF4D03F
+        elif 6 <= indiceRareza <= 15:
+            name, link, ID, rareza = card.getRandomCard()
+            while rareza != 4:
+                name, link, ID, rareza = card.getRandomCard()
+            color = 0x7D3C98
+
+        elif 26 <= indiceRareza <= 40:
+            name, link, ID, rareza = card.getRandomCard()
+            while rareza != 3:
+                name, link, ID, rareza = card.getRandomCard()
+            color = 0x3498DB
+
+        elif 41 <= indiceRareza <= 60:
+            name, link, ID, rareza = card.getRandomCard()
+            while rareza != 2:
+                name, link, ID, rareza = card.getRandomCard()
+            color = 0x229954
+
+        elif 61 <= indiceRareza <= 100:
+            name, link, ID, rareza = card.getRandomCard()
+            while rareza != 1:
+                name, link, ID, rareza = card.getRandomCard()
+            color = 0xF0F3F4
+
+        print(f"{ID} - {name}: {link} ")
+
+        Images.currentCard = card.Card(name,link,ID,rareza)
+        await self.print_card(name, link, color)
 
     @commands.command()
     async def set_timer(self, ctx, minn, maxx):
@@ -59,6 +100,37 @@ class Images(commands.Cog):
         await channel.send(embed=embed)
 
     @commands.command()
+    async def cards(self, ctx):
+
+        user = await self.bot.pg_con.fetchrow("SELECT * FROM users WHERE user_id = $1", str(ctx.message.author.id))
+        cardsArray = user['cards']
+        print(cardsArray)
+        embedString = ""
+        i = 1
+        for cardIn in cardsArray:
+            if cardIn != 0:
+                rareza = self.getRareza(card.cards[i].rareza)
+                embedString += f"ID:`{i}` - `{card.cards[i].name}` - Cantidad: `{cardIn}` - Rareza `{rareza}`\n"
+            i += 1
+
+        embed = discord.Embed(title="Cartas de {}".format(ctx.author), description=embedString).set_thumbnail(url=ctx.author.avatar_url)
+
+        channel = self.bot.get_channel(Images.channelID)
+        await channel.send(embed=embed)
+
+    def getRareza(self, index):
+        if index == 5:
+            return "Excepcional"
+        elif index == 4:
+            return "Leyenda"
+        elif index == 3:
+            return "Raro"
+        elif index == 2:
+            return "Uncommon"
+        else:
+            return "Comun"
+
+    @commands.command()
     async def get(self, ctx):
 
         if not Images.isActive:
@@ -78,19 +150,23 @@ class Images(commands.Cog):
             await self.bot.pg_con.execute("UPDATE users SET card_count = $1 WHERE user_id = $2", user['card_count'] + 1, str(
                 ctx.message.author.id))
 
+            print(Images.currentCard.id, user['cards'][Images.currentCard.id], ctx.message.author.id)
+            await self.bot.pg_con.execute("UPDATE users SET cards[$1] = $2 WHERE user_id = $3", Images.currentCard.id,
+                                          user['cards'][Images.currentCard.id] + 1, str(ctx.message.author.id)) # Updates the array
+
             newEmbed = discord.Embed(title="Nahhhh, el {} me atrapo.".format(
                 ctx.message.author),
                 description="Muy gay.",
                 color=0x00ff00)
             await Images.savedMessage.edit(embed=newEmbed)
 
-    async def print_card(self, name, link):
+    async def print_card(self, name, link, color):
 
         embedVar = discord.Embed(title="Aparecio un GAY",
                                  description="Un {} ha aparecido. Atrapalo con `_get`".format(name),
-                                 color=0x00ff00).set_image(url=link)
+                                 color=color).set_image(url=link)
         channel = self.bot.get_channel(Images.channelID)
-        Images.savedMessage = await channel.send(embed=embedVar, delete_after=30)
+        Images.savedMessage = await channel.send(embed=embedVar)
 
 
 def setup(bot):
